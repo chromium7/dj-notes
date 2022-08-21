162945e1df6566058e6fadddcb6090f00f5266d4
chromium7 <christofersaputra29@gmail.com>
2022-08-21 17:45:02 +0700

    Use custom user model

--- dj_notes/settings.py
+++ dj_notes/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Local apps
+    'dj_notes.apps.users',
+
     # 3rd party
     'django_extensions',
     'debug_toolbar',
@@ -133,6 +136,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Custom user model
+
+AUTH_USER_MODEL = 'users.User'
 
 
 # Django extensions
